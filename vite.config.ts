37f548e62ee138f2a448a14ad437a2456f3f3e4e
Rémi Connesson <remiconnesson@gmail.com>
2022-08-16 15:34:50 +0200

    Configure to run eslint alongside HMR

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,vue}"',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
