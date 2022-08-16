001e4aec775cf975d92e27c38123c574b26134f3
Rémi Connesson <remiconnesson@gmail.com>
2022-08-16 16:58:37 +0200

    Define the Period type, here used in navbar

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+// This allows us to create a type from the value of this array.
+const periods = ["Today", "This Week", "This Month"] as const;
+
+type Period = typeof periods[number];
+
+const selectedPeriod = ref<Period>("Today");
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+</script>
 
 <template>
   <nav class="is-primary panel">
     <span class="panel-tabs">
-      <a href="#">Today</a>
-      <a href="#">This Week</a>
-      <a href="#">This Month</a>
+      <a
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === selectedPeriod }"
+        @click="selectPeriod(period)"
+        href="#"
+        >{{ period }}</a
+      >
     </span>
   </nav>
 </template>
