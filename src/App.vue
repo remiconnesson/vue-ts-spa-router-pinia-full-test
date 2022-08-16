1df5a41a9d7eb276c355a152d6319e8cf80525e7
Rémi Connesson <remiconnesson@gmail.com>
2022-08-16 16:19:28 +0200

    Import a new component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Timeline from "@/components/Timeline.vue";
+</script>
 
 <template>
-  <h1>Hello, World!</h1>
+  <Timeline />
 </template>
 
 <style></style>
