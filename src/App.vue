436f8da621809f4bda4b770cee35b5c9af37f6e1
Rémi Connesson <remiconnesson@gmail.com>
2022-08-16 16:20:17 +0200

    Install Bulma CSS

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,6 @@ import Timeline from "@/components/Timeline.vue";
   <Timeline />
 </template>
 
-<style></style>
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
