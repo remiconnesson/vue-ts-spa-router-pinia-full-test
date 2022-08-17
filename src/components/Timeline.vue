2e1d4a7230206e131430e144840fecc55f934117
Rémi Connesson <remiconnesson@gmail.com>
2022-08-17 12:16:36 +0200

    Refactorize a computed property to a Pinia getter

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,39 +1,9 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import TimelineItem from "@/components/TimelineItem.vue";
-import { DateTime } from "luxon";
-import type { TimelinePost } from "@/posts";
 import { usePosts } from "@/stores/posts";
-// import type { Period } from "@/constants";
 import { periods } from "@/constants";
 
-// Stores
 const postsStore = usePosts();
-
-const filteredPosts = computed<TimelinePost[]>(() => {
-  return postsStore.ids
-    .map((id) => {
-      const post = postsStore.all.get(id);
-
-      if (!post) {
-        throw Error(`Post with id ${id} was expected but not found.`);
-      }
-
-      return {
-        ...post,
-        created: DateTime.fromISO(post.created),
-      };
-    })
-    .filter((post) => {
-      if (postsStore.selectedPeriod === "Today") {
-        return post.created >= DateTime.now().minus({ day: 1 });
-      } else if (postsStore.selectedPeriod === "This Week") {
-        return post.created >= DateTime.now().minus({ day: 7 });
-      } else if (postsStore.selectedPeriod === "This Month") {
-        return post.created >= DateTime.now().minus({ day: 30 });
-      }
-    });
-});
 </script>
 
 <template>
@@ -48,6 +18,10 @@ const filteredPosts = computed<TimelinePost[]>(() => {
         >{{ period }}</a
       >
     </span>
-    <TimelineItem v-for="post of filteredPosts" :key="post.id" :post="post" />
+    <TimelineItem
+      v-for="post of postsStore.filteredPosts"
+      :key="post.id"
+      :post="post"
+    />
   </nav>
 </template>
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,7 +1,8 @@
 import { defineStore } from "pinia";
-import type { Post } from "@/posts";
+import type { Post, TimelinePost } from "@/posts";
 import { today, thisWeek, thisMonth } from "@/posts";
 import type { Period } from "@/constants";
+import { DateTime } from "luxon";
 
 interface PostsState {
   ids: string[];
@@ -22,6 +23,33 @@ export const usePosts = defineStore("posts", {
   actions: {
     setSelectedPeriod(period: Period) {
       this.selectedPeriod = period;
+    },
+  },
+  // getters are like computed properties
+  getters: {
+    filteredPosts: (state): TimelinePost[] => {
+      return state.ids
+        .map((id) => {
+          const post = state.all.get(id);
+
+          if (!post) {
+            throw Error(`Post with id ${id} was expected but not found.`);
+          }
+
+          return {
+            ...post,
+            created: DateTime.fromISO(post.created),
+          };
+        })
+        .filter((post) => {
+          if (state.selectedPeriod === "Today") {
+            return post.created >= DateTime.now().minus({ day: 1 });
+          } else if (state.selectedPeriod === "This Week") {
+            return post.created >= DateTime.now().minus({ day: 7 });
+          } else if (state.selectedPeriod === "This Month") {
+            return post.created >= DateTime.now().minus({ day: 30 });
+          }
+        });
     },
   },
 });
