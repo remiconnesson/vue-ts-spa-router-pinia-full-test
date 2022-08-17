415ef2f04f270062aea58490b7a0eed669c6cd11
Rémi Connesson <remiconnesson@gmail.com>
2022-08-17 10:51:29 +0200

    Demonstrate implementing homemade state mgmt

---
+++ src/stores/posts.ts
@@ -0,0 +1,30 @@
+import { reactive, readonly } from "vue";
+
+interface PostsState {
+  foo: string;
+}
+
+export class PostsStore {
+  #state: PostsState;
+  constructor() {
+    this.#state = reactive<PostsState>({
+      foo: "foo",
+    });
+  }
+
+  getState() {
+    return readonly(this.#state);
+  }
+
+  updateFoo(foo: string) {
+    this.#state.foo = foo;
+  }
+}
+
+const store = new PostsStore();
+
+store.updateFoo("bar");
+
+export function usePosts() {
+  return store;
+}
