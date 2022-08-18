30379e41f992fc87e3ba83a13f54ee438002d680
Rémi Connesson <remiconnesson@gmail.com>
2022-08-18 18:33:31 +0200

    Setup the markdown editor

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
 import type { TimelinePost } from "@/posts";
 const props = defineProps<{
   post: TimelinePost;
 }>();
+
+const title = ref(props.post.title);
+
+const contentEditable = ref<HTMLDivElement>();
+
+onMounted(() => {
+  console.log(contentEditable.value?.innerText);
+});
 </script>
 
 <template>
   <div class="columns">
     <div class="column">
       <div class="field">
-        <div class="label">Post Title</div>
-        <input class="input" type="text" />
-        <p>{{ props }}</p>
+        <div class="label">New Post</div>
+        <input class="input" type="text" v-model="title" />
       </div>
     </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <div contenteditable ref="contentEditable">This is editable</div>
+    </div>
+    <div class="column">Preview</div>
   </div>
 </template>
