2d7e1a7b88690ca80497ea1bf7c16031ded7c03f
Rémi Connesson <remiconnesson@gmail.com>
2022-08-19 15:18:35 +0200

    Custom form input w/o double bind yet

---
+++ src/components/FormInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <div class="field">
+    <label class="label" :for="name">{{ name }}</label>
+    <div class="control"><input class="input" type="text" /></div>
+  </div>
+</template>
