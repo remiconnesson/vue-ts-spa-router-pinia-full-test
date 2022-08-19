a47839ebfa4a61f009d93537096b87ae0eeeee44
Rémi Connesson <remiconnesson@gmail.com>
2022-08-19 14:23:12 +0200

    Implement the foundation for a modal composable

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import Navbar from "@/components/Navbar.vue";
+import { useModal } from "@/composables/modal";
+
+const modal = useModal();
+
+const modalStyle = computed(() => {
+  return {
+    display: modal.show.value ? "block" : "none",
+  };
+});
 </script>
 
 <template>
+  <div class="modal" :style="modalStyle" style="color: white">
+    <div class="modal-background" @click="modal.hideModal">
+      <div class="modal-content">
+        <div id="modal"></div>
+      </div>
+    </div>
+    <button class="modal-close is-large" @click="modal.hideModal">
+      CLOSE THE MODAL
+    </button>
+  </div>
   <div class="section">
     <div class="container">
       <Navbar></Navbar>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,15 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+import { useModal } from "@/composables/modal";
+
+const { showModal } = useModal();
+</script>
+
 <template>
   <div class="navbar">
     <div class="navbar-end">
       <div class="buttons">
+        <button class="button" @click="showModal">Sign Up</button>
         <RouterLink :to="{ name: 'home' }" class="button">Home</RouterLink>
         <RouterLink :to="{ name: 'new-post' }" class="button"
           >New Post</RouterLink
@@ -9,4 +17,5 @@
       </div>
     </div>
   </div>
+  <Teleport to="#modal">This has been teleported.</Teleport>
 </template>
