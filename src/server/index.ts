645178270d205d86bac044e0ed5f7205e8f70c85
Rémi Connesson <remiconnesson@gmail.com>
2022-08-19 11:12:58 +0200

    Server endpoint to http-post new Posts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,23 @@
 import express from "express";
 import cors from "cors";
 import { today, thisWeek, thisMonth } from "../posts";
+import type { Post } from "../posts";
+
+const allPosts = [today, thisWeek, thisMonth];
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/posts", (req, res) => {
-  res.json([today, thisWeek, thisMonth]);
+  res.json(allPosts);
+});
+
+app.post<never, Post, Post>("/posts", (req, res) => {
+  const id = (Math.random() * 100000).toFixed();
+  const post = { ...req.body, id: id };
+  allPosts.push(post);
+  res.json(post);
 });
 
 app.listen(8000, () => {
