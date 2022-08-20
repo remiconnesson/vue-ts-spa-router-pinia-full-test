aa5fed49774e072e8840b558140e751934715d78
Rémi Connesson <remiconnesson@gmail.com>
2022-08-20 15:08:07 +0200

    Function to submit the form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,6 +3,7 @@ import { ref, computed } from "vue";
 import type { Status } from "@/validation";
 import { validate, required, length } from "@/validation";
 import FormInput from "@/components/FormInput.vue";
+import type { NewUser } from "@/users";
 
 const username = ref("");
 const usernameStatus = computed<Status>(() => {
@@ -15,12 +16,27 @@ const passwordStatus = computed<Status>(() => {
   const passwordRules = [required, length({ min: 8, max: 30 })];
   return validate(password.value, passwordRules);
 });
+
+const isInvalid = computed(() => {
+  return !(usernameStatus.value.valid && passwordStatus.value.valid);
+});
+
+function handleSubmit() {
+  if (isInvalid.value) return;
+  const newUser: NewUser = {
+    username: username.value,
+    password: password.value,
+  };
+
+  console.log(newUser);
+}
 </script>
 
 <template>
-  <form class="form">
+  <form class="form" @submit.prevent="handleSubmit">
     <FormInput name="Username" v-model="username" :status="usernameStatus" />
     <FormInput name="Password" v-model="password" :status="passwordStatus" />
+    <button class="button" :disabled="isInvalid">Submit</button>
   </form>
 </template>
 
