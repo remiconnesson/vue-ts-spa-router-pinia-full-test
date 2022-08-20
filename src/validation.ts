f026f43101c1677e7c9583ecb889ae64d9ea91c5
Rémi Connesson <remiconnesson@gmail.com>
2022-08-20 08:16:45 +0200

    In-source tests for validation logic

--- src/validation.ts
+++ src/validation.ts
@@ -21,3 +21,89 @@ export function validate(value: string, rules: Rule[]): Status {
   }
   return { valid: true };
 }
+
+/*
+ *  IN-SOURCE TEST SUITE
+ */
+
+// @ts-expect-error -- typescript thinks vitest property is undefined
+if (import.meta.vitest) {
+  // @ts-expect-error -- same as above -- TODO: open an issue on vitest repo
+  const { describe, it, expect, beforeAll } = import.meta.vitest; // dynamic import
+
+  describe("The required function", () => {
+    it("Should return a valid status when a value is passed", () => {
+      const result = required("I'm here");
+      expect(result.valid).toBe(true);
+      expect(result.message).not.toBeDefined();
+    });
+
+    it("Should return an invalid status when an empty string is passed", () => {
+      const result = required("");
+      expect(result.valid).toBe(false);
+      expect(result.message).toMatch(/required/);
+    });
+  });
+
+  describe("The validate function", () => {
+    // helper variables
+    let dummyValue: string; // the rules won't check the value passed
+    let alwaysValidRule: Rule; // a rule that always return a valid status
+    let makeInvalidRuleWithMessage: (message?: string) => Rule; // a factory for invalid rules with or witout message
+
+    beforeAll(() => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      alwaysValidRule = (message: string): Status => {
+        return { valid: true };
+      };
+
+      makeInvalidRuleWithMessage = (message?: string): Rule => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        return function alwaysInvalid(value: string): Status {
+          return { valid: false, message };
+        };
+      };
+
+      dummyValue = "to be valid, or not to be valid, that is the question...";
+    });
+
+    it("should return a status with true and w/o message if there's no rules", () => {
+      const status = validate(dummyValue, []);
+      expect(status.valid).toBe(true);
+      expect(status.message).not.toBeDefined();
+    });
+
+    it("should return invalid status when a value doesn't pass a rule", () => {
+      // without message
+      const status = validate(dummyValue, [
+        makeInvalidRuleWithMessage(undefined),
+      ]);
+
+      expect(status.valid).toBe(false);
+      expect(status.message).not.toBeDefined();
+    });
+
+    it("should return the error message the invalid rule emitted", () => {
+      const errorMessage = "this is an error message";
+      const status = validate(dummyValue, [
+        makeInvalidRuleWithMessage(errorMessage),
+      ]);
+
+      expect(status.valid).toBe(false);
+      expect(status.message).toBe(errorMessage);
+    });
+
+    it("should return invalid at the first invalid rule", () => {
+      const errorMessage = "this is an error message bis";
+      const status = validate(dummyValue, [
+        alwaysValidRule,
+        alwaysValidRule,
+        makeInvalidRuleWithMessage(errorMessage),
+        makeInvalidRuleWithMessage("This rule should not be attempted"),
+      ]);
+
+      expect(status.valid).toBe(false);
+      expect(status.message).toBe(errorMessage);
+    });
+  });
+}
