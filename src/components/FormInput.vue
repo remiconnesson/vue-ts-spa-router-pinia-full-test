271072959dbbccaa7e200c5e09110cebf0acf454
Rémi Connesson <remiconnesson@gmail.com>
2022-08-20 06:04:27 +0200

    Enable v-model for custom input composant

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 defineProps<{
   name: string;
+  modelValue: string;
+}>();
+
+defineEmits<{
+  (event: "update:modelValue", value: string): void;
 }>();
 </script>
 
 <template>
   <div class="field">
     <label class="label" :for="name">{{ name }}</label>
-    <div class="control"><input class="input" type="text" /></div>
+    <div class="control">
+      <input
+        class="input"
+        type="text"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+    </div>
   </div>
 </template>
