346894a78d583e5d338d212f72d56583290ea0d9
Rémi Connesson <remiconnesson@gmail.com>
2022-08-21 11:36:23 +0200

    Define the dev server endpoit for signin up users

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import { today, thisWeek, thisMonth } from "../posts";
 import type { Post } from "../posts";
+import type { NewUser, User } from "../users";
 
 const allPosts = [today, thisWeek, thisMonth];
+const allUsers: User[] = [];
 
 const app = express();
 app.use(cors());
@@ -18,6 +20,17 @@ app.post<never, Post, Post>("/posts", (req, res) => {
   const post = { ...req.body, id: id };
   allPosts.push(post);
   res.json(post);
+});
+
+app.post<never, Omit<User, "password">, NewUser>("/users", (req, res) => {
+  // push new user in DB
+  const id = (Math.random() * 100000).toFixed();
+  const user: User = { ...req.body, id: id };
+  allUsers.push(user);
+  // extract password before returning the new user
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password, ...userWithoutPassword } = user; // here password is declared to be extracted
+  res.json(userWithoutPassword);
 });
 
 app.listen(8000, () => {
