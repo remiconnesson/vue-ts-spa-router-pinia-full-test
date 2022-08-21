47d419f4c09cf0773806329e08fd524ff648aabe
Rémi Connesson <remiconnesson@gmail.com>
2022-08-21 18:54:15 +0200

    Extract logic to a reusable User Form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,66 +1,28 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import type { Status } from "@/validation";
-import { validate, required, length } from "@/validation";
-import FormInput from "@/components/FormInput.vue";
-import type { NewUser } from "@/users";
+import { ref } from "vue";
+import UserForm from "@/components/UserForm.vue";
 import { useUsers } from "@/stores/users";
+import type { NewUser } from "@/users";
 import { useModal } from "@/composables/modal";
 
 const usersStore = useUsers();
 const modal = useModal();
 
-const username = ref("");
-const usernameStatus = computed<Status>(() => {
-  const usernameRules = [required, length({ min: 4, max: 30 })];
-  return validate(username.value, usernameRules);
-});
+const error = ref("");
 
-const password = ref("");
-const passwordStatus = computed<Status>(() => {
-  const passwordRules = [required, length({ min: 8, max: 30 })];
-  return validate(password.value, passwordRules);
-});
+async function handleSubmit(newUser: NewUser) {
+  error.value = "";
 
-const isInvalid = computed(() => {
-  return !(usernameStatus.value.valid && passwordStatus.value.valid);
-});
-
-async function handleSubmit() {
-  if (isInvalid.value) return;
-  const newUser: NewUser = {
-    username: username.value,
-    password: password.value,
-  };
-
-  await usersStore.createUser(newUser);
+  try {
+    await usersStore.createUser(newUser);
+  } catch (e) {
+    error.value = "there was an error";
+  }
 
   modal.hideModal();
 }
 </script>
 
 <template>
-  <form class="form" @submit.prevent="handleSubmit">
-    <FormInput
-      name="Username"
-      type="text"
-      v-model="username"
-      :status="usernameStatus"
-    />
-    <FormInput
-      name="Password"
-      type="password"
-      v-model="password"
-      :status="passwordStatus"
-    />
-    <button class="button" :disabled="isInvalid">Submit</button>
-  </form>
+  <UserForm :error="error" @submit="handleSubmit" />
 </template>
-
-<style scoped>
-.form {
-  background: white;
-  padding: 30px;
-  margin-top: 50px;
-}
-</style>
