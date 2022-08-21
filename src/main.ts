39e78c91b9746f87c2f8d43e6b52655e96b38795
Rémi Connesson <remiconnesson@gmail.com>
2022-08-22 00:56:05 +0200

    Fix race conditions when refreshing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { router } from "@/router";
+import { usePosts } from "@/stores/posts";
+import { useUsers } from "@/stores/users";
 
 import App from "@/App.vue";
 
 const app = createApp(App);
 app.use(createPinia());
-app.use(router);
-app.mount("#app");
+
+const postsStore = usePosts();
+const usersStore = useUsers();
+
+Promise.all([postsStore.fetchPosts(), usersStore.authenticate()]).then(() => {
+  app.use(router);
+  app.mount("#app");
+});
